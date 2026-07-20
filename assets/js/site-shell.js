a9b864a1a5dc2e21ab73aebbf4fd4d3f042081ac
Anton Semenenko <adelayida@ukr.net>
2026-07-20 21:08:49 +0300

    Polish landing hero control chain

--- assets/js/site-shell.js
+++ assets/js/site-shell.js
@@ -1,5 +1,5 @@
 (() => {
-  const V = "20260720f";
+  const V = "20260720g";
   const path = (location.pathname || "").toLowerCase();
   const current = (path.split("/").pop() || "index.html").split("?")[0];
   const isHome = current === "index.html" || current === "" || current === "/";
