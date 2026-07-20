a4fa16222031471cc87738c7888dca1ac0700496
Anton Semenenko <adelayida@ukr.net>
2026-07-20 20:33:12 +0300

    Refine landing pipeline visual

--- assets/js/site-shell.js
+++ assets/js/site-shell.js
@@ -1,5 +1,5 @@
 (() => {
-  const V = "20260720d";
+  const V = "20260720e";
   const path = (location.pathname || "").toLowerCase();
   const current = (path.split("/").pop() || "index.html").split("?")[0];
   const isHome = current === "index.html" || current === "" || current === "/";
